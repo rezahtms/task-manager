98549234b58ee4079c951939e26952578fb1a068
rezahtms <rezahtms@gmail.com>
2026-02-01 16:14:36 +0330

    feat(tasks-page): add task page with basic structure

---
+++ app/(public)/tasks/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    absolute: "مدیریت وظایف",
+  },
+  description:
+    "وایب یک ابزار مدیریت وظایف شبیه Jira برای برنامه‌ریزی، پیگیری و همکاری تیم‌هاست.",
+};
+
+export default function Home() {
+  return <>hello world</>;
+}
